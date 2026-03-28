4cdf93c96ad2a037c742c46329ce2e5b455227af
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2026-03-28 19:26:18 -0300

    compact
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- compact.sh
+++ compact.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-jq -c . antennas.json > tmp.json && mv tmp.json antennas.json
+jq -c . antennas.json >tmp.json && mv tmp.json antennas.json
+jq -c . population.json >tmp.json && mv tmp.json population.json
